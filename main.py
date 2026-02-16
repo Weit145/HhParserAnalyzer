2704598349772a6c51454cda17315b58982eb1f0
Weit145 <weit.dima@yandex.ru>
2026-02-16 16:53:38 +0300

    Fix :bug:

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 # https://spb.hh.ru/search/vacancy?text=Кассир&ored_clusters=true&order_by=publication_time&page=0
 
 class Parser:
-    def __init__(self, select_work, mx:int = 200):
+    def __init__(self, select_work, mx:int = 20):
         self.mx = mx
         
         self.work = self.validate(select_work)
@@ -27,29 +27,55 @@ class Parser:
             print(self.url)
             if html is None:
                 break
-            
-            soup = BeautifulSoup(html, "html.parser")
-            vacancy_cards = soup.find_all("div", {"data-qa": "vacancy-serp__vacancy"})
-            for card in vacancy_cards:
-                spans = card.find_all('span')
-                for span in spans:
-                    text = span.get_text(strip=True)
-                    if text =="":
-                        print(self.salaries)
-                        return
-                    if '₽' in text and "за\xa0месяц" in text and len(self.salaries) < self.mx:
-                        cleaned = text.replace('\xa0', ' ').strip()
-                        cleaned = cleaned.replace('\u202f', '')
-                        numbers = re.findall(r'\d+', cleaned)
-                        if len(numbers)==2:
-                            numbers=(int(numbers[0])+int(numbers[1]))/2
-                            self.salaries.append(int(numbers))
-                            continue
-                        self.salaries.append(int(numbers[0]))
-                        break
+            try:
+                soup = BeautifulSoup(html, "html.parser")
+                self.find_price(soup)
+            except Exception:
+                break
             page += 1
         print(self.salaries)
+    
+    def find_price(self, soup:BeautifulSoup):
+        vacancy_cards = soup.find_all("div", {"data-qa": "vacancy-serp__vacancy"})
+        if not vacancy_cards:
+            raise ValueError("No vacancies found")
+        
+        for card in vacancy_cards:
+                
+                if len(self.salaries) >= self.mx:
+                    break
+
+                fn=card.find(class_="magritte-text___pbpft_4-4-4 magritte-text_style-primary___AQ7MW_4-4-4 magritte-text_typography-label-1-regular___pi3R-_4-4-4")
+                if fn is None:
+                    continue
             
+                price = self.check_valute(fn.text)
+                if price is None:
+                    continue
+
+                self.salaries.append(price)
+
+    def check_valute(self, fn:str)->int | None:
+        if "₽" in fn:
+            return self.check_price(fn)
+        elif "$" in fn:
+            return self.check_price(fn, 76)
+        elif "€" in fn:
+            return self.check_price(fn, 91)
+        else:
+            return None
+
+    def check_price(self, price:str, x:int=1)->int | None:
+        price = price.replace('\xa0', ' ').strip()
+        price = price.replace('\u202f', '')
+        numbers = re.findall(r'\d+', price)
+        n = len(numbers)
+        if n == 2:
+            return ((int(numbers[0]) + int(numbers[1])) // 2)*x
+        elif n == 1:
+            return int(numbers[0])*x
+        else:
+            return None
     
     def validate(self, select_work)->str:
         if select_work == "":
