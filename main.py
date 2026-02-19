07285826a5e73c8ddf2dfb96c1cc0b7307ba41f7
Weit145 <weit.dima@yandex.ru>
2026-02-19 15:38:44 +0300

    clear code :memo:

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ class Parser:
         self.mx = mx
         
         self.work = self.validate(select_work)
-        self.url = f"https://spb.hh.ru/search/vacancy?text={self.work}&ored_clusters=true&order_by=publication_time&page="
+        self.url =f"https://spb.hh.ru/search/vacancy?text={self.work}&ored_clusters=true&order_by=publication_time&page="
         
         self.session = requests.Session()
         self.session.headers.update(
@@ -20,20 +20,24 @@ class Parser:
         
     def run(self):
         page = 0
+
         while len(self.salaries) < self.mx:
             self.url = f"https://spb.hh.ru/search/vacancy?text={self.work}&ored_clusters=true&order_by=publication_time&page={page}"
             
             html = self.fetch()
             print(self.url)
+
             if html is None:
                 break
+
             try:
                 soup = BeautifulSoup(html, "html.parser")
                 self.find_vacancy(soup)
             except Exception:
                 break
+
             page += 1
-        print(self.salaries)
+        return self.salaries
     
     def find_vacancy(self, soup:BeautifulSoup):
         vacancy_cards = soup.find_all("div", {"data-qa": "vacancy-serp__vacancy"})  
@@ -51,23 +55,18 @@ class Parser:
             return
         
         for block in salary_block:
-            if "₽" in block.text or "$" in block.text or "€" in block.text:
-                price=self.check_valute(block.text)
-                if price is not None:
-                    self.salaries.append(price)
+            price=None
+            
+            if "₽" in block.text:
+                price=self.check_price(block.text)
+            elif "$" in block.text:
+                price=self.check_price(block.text, 76)
+            elif "€" in block.text:
+                price=self.check_price(block.text, 91)
+            if price is not None:
+                self.salaries.append(price)
                 return
 
-    
-
-    def check_valute(self, fn:str)->int | None:
-        if "₽" in fn:
-            return self.check_price(fn)
-        elif "$" in fn:
-            return self.check_price(fn, 76)
-        elif "€" in fn:
-            return self.check_price(fn, 91)
-        else:
-            return None
 
     def check_price(self, price:str, x:int=1)->int | None:
         price = price.replace('\xa0', ' ').strip()
@@ -84,16 +83,18 @@ class Parser:
     def validate(self, select_work)->str:
         if select_work == "":
             raise ValueError("No work selected")
-        if select_work [0] == " ":
-            select_work = select_work[1:]
-        if select_work [-1] == " ":
-            select_work = select_work[:-1]
+        
+        select_work = select_work.rstrip()
+
         if "+" in select_work:
             select_work = select_work.replace("+", "%2B")
+
         return select_work.replace(" ", "+")
     
     def fetch(self) -> str | None:
         try:
+            if self.url is None:
+                raise
             resp = self.session.get(self.url, timeout=10)
             if resp.status_code != 200:
                 return None
@@ -106,7 +107,7 @@ class Parser:
 def main():
     select_work = input()
     parser = Parser(select_work)
-    parser.run()
+    print(parser.run())
     
 if __name__ == "__main__":
     main()
